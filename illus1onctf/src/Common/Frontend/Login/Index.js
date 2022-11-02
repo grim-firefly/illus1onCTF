841c0b003b80f3b3d7cc602e7f13989b825b94e3
MD Khairul Hasan Sajid <khairulhasansajid@gmail.com>
2022-11-03 00:16:30 +0600

    log in page almost complete

--- illus1onctf/src/Common/Frontend/Login/Index.js
+++ illus1onctf/src/Common/Frontend/Login/Index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Input from './../Common/Input/Index';
 import { TfiUser } from 'react-icons/tfi';
 import { RiLockPasswordLine } from 'react-icons/ri';
+import { FcGoogle } from 'react-icons/fc';
+import { BsFacebook, BsGithub } from 'react-icons/bs';
 import s from './style.module.css';
 import formlogo from './../../../Assets/Images/illus1onCTF1.png';
 import { Link } from 'react-router-dom';
@@ -20,20 +22,26 @@ const Login = () => {
 						<div className='mb-3'>
 							<Input type="password" name="password" placeholder="Password" icon={RiLockPasswordLine} />
 						</div>
-						<div className=''>
-							<button className='btn btn-primary' >Login</button>
+						<div className='d-flex justify-content-center '>
+							<button className='btn btn-primary px-5' >Login</button>
 
 						</div>
-						<div className='d-flex flex-row-reverse'>
+						<div className='d-flex flex-row-reverse mt-1'>
 							<Link className={`${s.fogotpassword}`}>Forgot Password ?</Link>
 						</div>
 
 					</form>
-					<div>
+					<div className='mt-2'>
 						<p className={`${s.separator}`}><span className={`${s.separatortext}`}>OR</span></p>
 					</div>
-					<div className=''>
-						<p className={`${s.formFooterText}`}>Don't have an account ? <Link className={`${s.formFooterLink}`}>Sign Up</Link></p>
+
+					<div className={`${s.othersLogin}`}>
+						<Link to="/" className={`${s.socialmedia}`}><FcGoogle /> </Link>
+						<Link to="/" className={`${s.socialmedia}`}><BsFacebook /> </Link>
+						<Link to="/" className={`${s.socialmedia}`}><BsGithub /> </Link>
+					</div>
+					<div className={`${s.reference}`}>
+						<p>Need an account ? <Link className={`${s.formFooterLink}`}>Sign Up</Link></p>
 					</div>
 				</div>
 

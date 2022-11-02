a018e623c62d5f6e41e7363be084ac94ba511ff6
MD Khairul Hasan Sajid <khairulhasansajid@gmail.com>
2022-11-02 17:07:31 +0600

    change git ignore

--- illus1onctf/src/Common/Frontend/Header/Index.js
+++ illus1onctf/src/Common/Frontend/Header/Index.js
@@ -1,10 +1,28 @@
 import React from 'react';
-
-const Header= () => {
+import s from './style.module.css';
+import { Link } from 'react-router-dom';
+const Header = () => {
 	return (
-		<div>
-			This is Header
-		</div>
+
+		<nav className={`${s.navbar}`}>
+			<div className='container'>
+
+				<div className={`${s.nav}`}>
+					<div>
+						<Link className={`${s.brand}`} href="/">illus1on<span className={`${s.logo_end}`}>CTF</span></Link>
+					</div>
+					<div>
+						<Link className={`${s.navLink}`} href="/challenges">Challenges</Link>
+						<Link className={`${s.navLink}`} href="/leaderboard">Leaderboard</Link>
+						<Link className={`${s.navLink}`} href="/profile">Profile</Link>
+
+					</div>
+				</div>
+			</div>
+
+
+		</nav>
+
 	);
 }
 export default Header;
--- illus1onctf/src/index.js
+++ illus1onctf/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>

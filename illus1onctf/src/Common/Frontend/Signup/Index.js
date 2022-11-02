48e8c42d62fc377aeffa886bed315c73e965c1d8
MD Khairul Hasan Sajid <khairulhasansajid@gmail.com>
2022-11-03 01:01:04 +0600

    Sign up pages added

--- illus1onctf/src/Common/Frontend/Signup/Index.js
+++ illus1onctf/src/Common/Frontend/Signup/Index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Input from './../Common/Input/Index';
+import Input from '../Common/Input/Index';
 import { TfiUser } from 'react-icons/tfi';
-import { RiLockPasswordLine } from 'react-icons/ri';
+import { RiLockPasswordLine, RiLockPasswordFill } from 'react-icons/ri';
 import { FcGoogle } from 'react-icons/fc';
 import { BsFacebook, BsGithub } from 'react-icons/bs';
+import {TfiEmail} from 'react-icons/tfi';
 import s from './style.module.css';
 import formlogo from './../../../Assets/Images/illus1onCTF.png';
 import { Link } from 'react-router-dom';
-const Login = () => {
+const Signup = () => {
 	return (
 		<div className='container'>
 			<div className='row mt-5'>
@@ -17,21 +18,23 @@ const Login = () => {
 					</div>
 					<form action="" method="post" className='' >
 						<div className='mb-3'>
-							<Input type="email" name="email" placeholder="username or email" icon={TfiUser} />
+							<Input type="text" name="username" placeholder="Username" icon={TfiUser} />
+						</div>
+						<div className='mb-3'>
+							<Input type="email" name="email" placeholder="Email" icon={TfiEmail} />
 						</div>
 						<div className='mb-3'>
 							<Input type="password" name="password" placeholder="Password" icon={RiLockPasswordLine} />
 						</div>
-						<div className='d-flex justify-content-center '>
-							<button className='btn btn-primary px-5' >Login</button>
-
+						<div className='mb-3'>
+							<Input type="password" name="confim_password" placeholder="Confirm Password" icon={RiLockPasswordFill} />
 						</div>
-						<div className='d-flex flex-row-reverse mt-1'>
-							<Link className={`${s.fogotpassword}`}>Forgot Password ?</Link>
+						<div className='d-flex justify-content-center '>
+							<button className='btn btn-primary px-5' >Sign Up</button>
 						</div>
 
 					</form>
-					<div className='mt-2'>
+					<div className='mt-4'>
 						<p className={`${s.separator}`}><span className={`${s.separatortext}`}>OR</span></p>
 					</div>
 
@@ -41,7 +44,7 @@ const Login = () => {
 						<Link to="/" className={`${s.socialmedia}`}><BsGithub /> </Link>
 					</div>
 					<div className={`${s.reference}`}>
-						<p>Need an account ? <Link to='/signup' className={`${s.formFooterLink}`}>Sign Up</Link></p>
+						<p>Already a User ? <Link to='/login' className={`${s.formFooterLink}`}>Login</Link></p>
 					</div>
 				</div>
 
@@ -49,4 +52,4 @@ const Login = () => {
 		</div>
 	);
 }
-export default Login;
+export default Signup;

e629344ec559a60235901aa02ae915690937321b
MD Khairul Hasan Sajid <khairulhasansajid@gmail.com>
2022-11-05 00:46:43 +0600

    responsiveness added

--- illus1onctf/src/Pages/Challenges/Challenges.js
+++ illus1onctf/src/Pages/Challenges/Challenges.js
@@ -18,26 +18,58 @@ const data = [
 		"solved": 0,
 		"solve": 102,
 		"like": 101
-	}
+	},
+	{
+		"category": "Web Exploitation",
+		"point": 10,
+		"title": "Nice netcat...Nice netcat...Nice netcat...Nice netcat...",
+		"solved": 1,
+		"solve": 10,
+		"like": 10
+	},
+	{
+		"category": "Web Exploitation",
+		"point": 10,
+		"title": "Nice Python...",
+		"solved": 0,
+		"solve": 102,
+		"like": 101
+	},
+	{
+		"category": "Web Exploitation",
+		"point": 10,
+		"title": "Nice netcat...Nice netcat...Nice netcat...Nice netcat...",
+		"solved": 1,
+		"solve": 10,
+		"like": 10
+	},
+	{
+		"category": "Web Exploitation",
+		"point": 10,
+		"title": "Nice Python...",
+		"solved": 0,
+		"solve": 102,
+		"like": 101
+	},
 ];
 
 const Challenges = () => {
 	return (
 		<div className='container'>
-			<div className="row mt-2">
-				<div className="col-md-5 col-lg-4 col-xl-3  ">
+			<div className="row">
+				<div className="col-md-5 col-lg-4 col-xl-3  mt-2">
 					<div className="">
 						<Filters />
 					</div>
 
 				</div>
-				<div className="col-md-7 col-lg-8 col-xl-9">
+				<div className="col-md-7 col-lg-8 col-xl-9 mt-2">
 					<div className='row'>
 
 						{
 							data.map((item, index) => (
 
-								<div key={index} className='col-4 mb-1' >
+								<div key={index} className='col-xl-4 col-lg-6  col-12 mb-1' >
 									<ChallengeCard data={item} />
 								</div>
 							))

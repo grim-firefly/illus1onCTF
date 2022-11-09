13691cbb379a7b5aaf9f8b0b138a67727e30e206
MD Khairul Hasan Sajid <khairulhasansajid@gmail.com>
2022-11-09 18:35:42 +0600

    added data

---
+++ illus1onctf/src/data/Challenges.js
@@ -0,0 +1,33 @@
+export const challenges=[
+	{
+		"id": 1,
+		"category": 1,
+		"name": "XSS Attack",
+		"points": 100,
+		"author": "Sajid",
+		'solved': false,
+		'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat odit facere! Porro, enim nam! Recusandae, cum beatae consequuntur perferendis quo commodi aliquam? Totam velit consequatur officiis ipsa quos perferendis? Corrupti ipsa voluptatum ducimus nesciunt ea. Est voluptatibus dolorum debitis, ipsam dolor quas quo optio saepe ducimus minus corporis quisquam sit commodi error voluptate laborum perferendis. Ut minima officia veniam! Voluptatibus error, quaerat id laborum, magnam mollitia obcaecati, voluptatem nisi earum facere quos omnis? Optio necessitatibus dolore, iure quos sit, temporibus culpa unde odio beatae illum, ut reiciendis repudiandae tempora.',
+		'likes': 0,
+	},
+	{
+		"id": 2,
+		"category": 2,
+		"name": "SQL Attack",
+		"points": 100,
+		"author": "MD Khairul Hasan",
+		'solved': true,
+		'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat odit facere! Porro, enim nam! Recusandae, cum beatae consequuntur perferendis quo commodi aliquam? Totam velit consequatur officiis ipsa quos perferendis? Corrupti ipsa voluptatum ducimus nesciunt ea. Est voluptatibus dolorum debitis, ipsam dolor quas quo optio saepe ducimus minus corporis quisquam sit commodi error voluptate laborum perferendis. Ut minima officia veniam! Voluptatibus error, quaerat id laborum, magnam mollitia obcaecati, voluptatem nisi earum facere quos omnis? Optio necessitatibus dolore, iure quos sit, temporibus culpa unde odio beatae illum, ut reiciendis repudiandae tempora.',
+		'likes': 0,
+	},
+	{
+		"id": 3,
+		"category": 3,
+		"name": "revershell Attack",
+		"points": 100,
+		"author": "MD Khairul Hasan",
+		'solved': false,
+		'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat odit facere! Porro, enim nam! Recusandae, cum beatae consequuntur perferendis quo commodi aliquam? Totam velit consequatur officiis ipsa quos perferendis? Corrupti ipsa voluptatum ducimus nesciunt ea. Est voluptatibus dolorum debitis, ipsam dolor quas quo optio saepe ducimus minus corporis quisquam sit commodi error voluptate laborum perferendis. Ut minima officia veniam! Voluptatibus error, quaerat id laborum, magnam mollitia obcaecati, voluptatem nisi earum facere quos omnis? Optio necessitatibus dolore, iure quos sit, temporibus culpa unde odio beatae illum, ut reiciendis repudiandae tempora.',
+		'likes': 0,
+	},
+
+]

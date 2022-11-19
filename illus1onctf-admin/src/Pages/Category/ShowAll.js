b29bcf22ed7e658744b34be0970ebb5d592417b4
MD Khairul Hasan Sajid <khairulhasansajid@gmail.com>
2022-11-20 01:48:25 +0600

    Added search features and combined search and index

--- illus1onctf-admin/src/Pages/Category/ShowAll.js
+++ illus1onctf-admin/src/Pages/Category/ShowAll.js
@@ -8,7 +8,6 @@ import { FaPlus } from 'react-icons/fa';
 import { useEffect, useState } from 'react';
 import { Table } from 'antd';
 import { PropagateLoader } from 'react-spinners';
-import { set } from 'immer/dist/internal';
 
 const ShowAllCategory = () => {
 	const column = [
@@ -51,39 +50,29 @@ const ShowAllCategory = () => {
 	const [page, setPage] = useState(1);
 	const [pageSize, setPageSize] = useState(10);
 	const [isLoading, setIsLoading] = useState(false);
-	const [isPaginationLoading, setIsPaginationLoading] = useState(false);
 	const [totalData, setTotalData] = useState(0);
 	const [categories, setCategories] = useState([]);
-	useEffect(() => {
-		setIsPaginationLoading(true)
-		const fetchTotalDataCount = async () => {
-			const response = await axios.get('/categories/total');
-			return response.data;
-		}
-		fetchTotalDataCount().then(data => {
-			setTotalData(data.total)
-			setIsPaginationLoading(false)
-		})
+	const [refresher, setRefresher] = useState(false);
+	const [search, setSearch] = useState('');
 
-	}, [])
 
-	useEffect(() => {
-		setIsLoading(true)
-		const fetchData = async () => {
-			const response = await axios.get('/categories', {
-				params: {
-					page,
-					pageSize
-				}
-			});
-			return response.data;
-		}
 
-		fetchData().then(data => {
-			setCategories(data.categories)
-			setIsLoading(false)
-		})
-	}, [page, pageSize])
+	// useEffect(() => {
+	// 	setIsLoading(true)
+	// 	const fetchData = async () => {
+	// 		const response = await axios.get('/categories', {
+	// 			params: {
+	// 				page,
+	// 				pageSize
+	// 			}
+	// 		});
+	// 		return response.data;
+	// 	}
+	// 	fetchData().then(data => {
+	// 		setCategories(data.categories)
+	// 		setIsLoading(false)
+	// 	})
+	// }, [page, pageSize, refresher])
 
 	const handleDelete = (record) => {
 		setIsLoading(true)
@@ -94,63 +83,76 @@ const ShowAllCategory = () => {
 		}
 		deleteData().then(data => {
 			setCategories(categories.filter(category => category.id !== record.id))
-			setIsLoading(false)
+			// setIsLoading(false)
+			setRefresher(!refresher)
 		})
 
 	}
+	useEffect(() => {
+		setIsLoading(true)
+		setPage(1)
+		const fetchData = async () => {
+			const response = await axios.get('/categories', {
+				params: {
+					page,
+					pageSize,
+					search
+				}
+			});
+			return response.data;
+		}
+		fetchData().then(data => {
+			setCategories(data.categories)
+			setTotalData(data.total)
+			setIsLoading(false)
+		})
+
+	}, [search, page, pageSize, refresher])
+
+	const handleSearch = (e) => {
+		setTimeout(() => {
+			setSearch(e.target.value)
+		}, 500)
+	}
+
 
 	return (<>
-		{
-			isPaginationLoading && < PropagateLoader loading={isPaginationLoading} color={"#1B98F5"} cssOverride={{
-				display: "block",
-				position: "absolute",
-				left: "50%",
-				top: "50%",
-				transform: "translate(-50%, -50%)",
-				zIndex: "999999999",
-				borderColor: "red",
-			}
-			} size={25} />
-		}
-		{
-			!isPaginationLoading &&
-			< div >
 
+		<div >
 
-				<div className='row align-items-center p-2'>
-					<div className='col-2 '>
-						<Link to="create"><OutlineButton title="Add" Icon={FaPlus} /></Link>
-					</div>
-					<div className='offset-xl-7 offset-lg-6 offset-md-5  col-md-5 col-lg-4 col-xl-3 offset-3 col-7'>
-						<Input placeholder="Seach" icon={BsSearch} />
-					</div>
+			<div className='row align-items-center p-2'>
+				<div className='col-2 '>
+					<Link to="create"><OutlineButton title="Add" Icon={FaPlus} /></Link>
 				</div>
-				<div className='p-2'>
-					<Table
-						columns={column}
-						loading={{
-							spinning: isLoading,
-							indicator: <PropagateLoader color={"#1B98F5"} />
-						}}
-
-						dataSource={categories}
-						rowKey={record => record.id}
-						pagination={{
-							total: totalData,
-							onChange: (page) => {
-								setPage(page)
-							},
-							onShowSizeChange: (current, size) => {
-								setPage(current)
-								setPageSize(size)
-							}
-						}}
-
-					/>
+				<div className='offset-xl-7 offset-lg-6 offset-md-5  col-md-5 col-lg-4 col-xl-3 offset-3 col-7'>
+					<Input placeholder="Seach" icon={BsSearch} onChange={handleSearch} />
 				</div>
+			</div>
+			<div className='p-2'>
+				<Table
+					columns={column}
+					loading={{
+						spinning: isLoading,
+						indicator: <PropagateLoader color={"#1B98F5"} />
+					}}
 
-			</div >
-		}
+					dataSource={categories}
+					rowKey={record => record.id}
+					pagination={{
+						total: totalData,
+						onChange: (page) => {
+							setPage(page)
+						},
+						onShowSizeChange: (current, size) => {
+							setPage(current)
+							setPageSize(size)
+						}
+					}}
+
+				/>
+			</div>
+
+		</div >
 
 	</>
 
